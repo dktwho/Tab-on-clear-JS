4f0439b437b5de817832d15673d580d17c91b976
Alexandr Korobov <100015046+dktwho@users.noreply.github.com>
2022-08-19 10:45:01 +0300

    Done

---
+++ 8_tabs_create.js
@@ -0,0 +1,30 @@
+
+const tabsBtn = document.querySelectorAll('.tabs__nav-btn')
+const tabsItems = document.querySelectorAll('.tabs__item')
+
+tabsBtn.forEach(onTabClick) 
+
+function onTabClick(item) {
+  item.addEventListener('click', ()=> {
+    let currentBtn = item
+    let tabId = currentBtn.getAttribute("data-tab")
+    let currenTab = document.querySelector(tabId)
+
+    if(!currentBtn.classList.contains('active')) {
+
+      tabsBtn.forEach((item) => {
+        item.classList.remove('active')
+      })
+  
+      tabsItems.forEach((item) => {
+        item.classList.remove('active')
+      })
+      
+      currentBtn.classList.add('active')
+      currenTab.classList.add('active')
+    } 
+
+  })
+  
+}
+document.querySelector('.tabs__nav-btn').click()
